521dd2e5a8b21dba38183ce80e787af08b21671b
warlockz <yogerocks@gmail.com>
2017-08-03 06:59:13 +0530

    Added exception handling in string class

--- string_class_copy_constructor.cpp
+++ string_class_copy_constructor.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <stdio.h>
 #include <sstream>
+#include <exception>
 
 using namespace std;
 
@@ -28,43 +29,7 @@ public:
 		strcpy(this->str,s.str);
 	}
 
-	String& operator = (const String& s)
-	{
-		if(this != &s)
-		{
-			this->str = new char[strlen(s.str) + 1];
-			strcpy(this->str,s.str);
-		}
-		return *this;
-	}
 
-	//Functors operattor
-	char* operator()() const
-	{
-		return (this->str);
-	}
-	//Coversion Opertor to typename of(char*)
-	
-	operator char* () const
-	{
-		return (this->str);
-	}
-	
-	/*
-	//To avoid Class String compare or assign with char* variable directly so use explicit keyword
-	//eg:
-	// String str("A");
-	// char* s1 = "D";
-	// : if(str == s1)
-	//if you wantto compare then type cast it
-	// if(str == static_cast<String>s1)
-	
-	explicit operator char* () const
-	{
-		return (this->str);
-	}
-
-	*/
 	static char* IntToStr(int value)
 	{
 		//using sprintf
@@ -95,10 +60,99 @@ public:
 		strcpy(this->str,str);
 	}
 
+	String& operator = (const String& s)
+	{
+		if(this != &s)
+		{
+			this->str = new char[strlen(s.str) + 1];
+			strcpy(this->str,s.str);
+		}
+		return *this;
+	}
+
+	//Functors operattor
+	char* operator()() const
+	{
+		return (this->str);
+	}
+
+	//Coversion Opertor to typename of(char*) if we overload << insertion operator it will be called 
+	// in such case conversion operator will not be called
+	operator char* () const
+	{
+		return (this->str);
+	}
+	
+	/*
+	//To avoid Class String compare or assign with char* variable directly so use explicit keyword
+	//eg:
+	// String str("A");
+	// char* s1 = "D";
+	// : if(str == s1)
+	//if you wantto compare then type cast it
+	// if(str == static_cast<String>s1)
+	
+	explicit operator char* () const
+	{
+		return (this->str);
+	}
+
+	*/
+
+	// Overloading operator << insertion operator as friend global function
+	friend ostream& operator << (ostream& out,const String& s)
+	{
+		out << "Internal string : ";
+		out << s.getStr();
+		return out;
+	}
+
+	//return char at certain position overloading subscript operator
+	char& operator [] (int index)
+	{
+		class StringIndexInvalidException : public exception
+		{
+
+		public:
+			const char* what()
+			{
+				cout << "String Index Out of Bound exception \n";
+			}
+
+		};
+
+		try
+		{
+			if(index >= strlen(str))
+			{
+				StringIndexInvalidException e;
+				throw e;
+			}
+			else
+			{
+				return str[index];
+			}
+		}
+		catch(StringIndexInvalidException& e)
+		{
+			cout << e.what();
+		}
+	}
+
 private:
 	char *str;
 	
 };
+
+// Overloading operator >> extraction operator as without friend global function
+istream& operator >> (istream & in,String& s)
+{
+	cout << "Please enter string ? : \n";
+	char *str =  new char[256];
+	in >> str;
+	s.setStr(str);
+	return in;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -106,13 +160,35 @@ int main(int argc, char const *argv[])
 	cout << "Functor String operator () : ";
 
 	cout << str() <<"\n";
-	cout <<"Conversion operator : "<< str <<"\n";
+	char *val = str;
+	cout <<"Conversion operator : "<< val <<"\n";
 	//char * explicitstr = str;
 	//cout << "Exp str " << explicitstr;
 	str.setStr("321");
 	
 	cout << "String getstr : ";
 	cout << str.getStr() <<"\n";
+
+	cout << "Overloading Extraction >> operator : ";
+	cin >> str;
+
+	cout << "Overloading Insertion  << operator : ";
+	cout << str << endl;
+
+
+	cout << "Overloading subscript [] operator : ";
+	cout << "Enter index to change char ? :";
+	int index = 0;
+	cin >> index;
+	try 
+	{
+		str[index] = 'q';
+	}
+	catch(...)
+	{
+		cout << "exception  caught default"; 
+	}
+	cout << str << endl;
 
 
 	return 0;
